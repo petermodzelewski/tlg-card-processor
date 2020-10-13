5688296658248714fe4df3acff83770d8fec5829
KeYeR <peter.modzelewski@gmail.com>
2020-10-13 22:03:44 +0200

    update to new patch (gwent) script to get all the images for lor

---
+++ download_lor.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+rm -rf result/lor/cards
+mkdir -p result/lor/cards
+
+declare -a SetArray=("1" "2" "3")
+
+for val in ${SetArray[@]}; do
+  SET="set$val-en_us"
+  FILE="$SET.zip"
+  rm -rf result/lor/$FILE
+  curl https://dd.b.pvp.net/latest/$FILE --output result/lor/$FILE
+  unzip result/lor/$FILE -d result/lor/$SET
+  mv result/lor/$SET/en_us/img/cards/*.png result/lor/cards/
+done
+
+
